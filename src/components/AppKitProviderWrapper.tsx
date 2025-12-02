b5253e2f0e56a0ab91f54121a83aaafc52a5eec0
phessophissy <adefesobi@yahoo.com>
2025-12-02 10:09:14 +0100

    Remove AppKitProvider wrapper for production build

--- src/components/AppKitProviderWrapper.tsx
+++ src/components/AppKitProviderWrapper.tsx
@@ -1,25 +1,17 @@
 'use client';
 
 import React from 'react';
-import dynamic from 'next/dynamic';
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { wagmiAdapter } from '@/lib/appkit';
 
 const queryClient = new QueryClient();
 
-const DynamicAppKitProvider = dynamic(
-  () => import('@reown/appkit/react').then((mod) => mod.AppKitProvider),
-  { ssr: false }
-);
-
 export default function AppKitProviderWrapper({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={wagmiAdapter.wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <DynamicAppKitProvider>
-          {children}
-        </DynamicAppKitProvider>
+        {children}
       </QueryClientProvider>
     </WagmiProvider>
   );
